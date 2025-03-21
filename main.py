093860e731bca0b67ed0582b2432e07aefb1f8fc
Wistak <piotr.slomczynski@erpatech.com.pl>
2025-03-21 15:12:37 +0100

    Complete task_2

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# ZAD 1
+
 numbers = [5,32,56,2,2,16,7,10,23,100]
 mean_number = 0
 
@@ -15,3 +17,21 @@ for num in numbers:
     sum += num
 mean_number = sum / len(numbers)
 print(mean_number)
+
+# ZAD 2
+
+def build_bridge(chunk,goal):
+    flag = True
+    while goal > 0:
+        goal -= chunk
+        if goal <= 0:
+            flag = True
+            break 
+        goal -= chunk/2
+        flag = False     
+    return flag
+
+
+print(build_bridge(2,20))
+print(build_bridge(2,18))
+
